6baec56195c44ac1b80c308d2faf79641f4cfe20
gowtham-source <75899822+gowtham-source@users.noreply.github.com>
2023-05-02 20:04:25 +0530

    initial

--- try.py
+++
@@ -1,7 +0,0 @@
-name = input()
-search_query = name.replace(' ', '+')
-# base_url = 'https://www.amazon.com/s?k={0}'.format(search_query)
-base_url = "https://www.flipkart.com/search?q={0}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off".format(
-    search_query)
-print(search_query)
-print(base_url)
